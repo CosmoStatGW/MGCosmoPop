R0 and correct normalization included

dataFarr.py
@@ -1,46 +1,94 @@
 import numpy as np
 import h5py
 import os
+from config import *
+from utils import *
 
 
-###############################################
-#  GLOBAL VARIABLES
-###############################################
 
 
-dirName  = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-dataPath=os.path.join(dirName, 'data')
 
-nSamplesUse=100
-nObsUse=2
-nInjUse=100
+def originalMassPrior(m1z, m2z):
+    return np.ones(m1z.shape)
+
+
+def originalDistPrior(dL):
+    return np.ones(dL.shape)
+
+
 
 ###############################################
 #  LOADING DATA
 ###############################################
 
-
+def load_data(dataset_name):
+    if dataset_name=='mock':
+        return load_mock_data()
+    elif dataset_name=='O3':
+        return load_O3_data()
+    
 
 
 def load_mock_data():
     with h5py.File(os.path.join(dataPath,'observations.h5'), 'r') as phi: #observations.h5 has to be in the same folder as this code
-   
-        m1det_samples = np.array(phi['posteriors']['m1det'])[:nObsUse, :nSamplesUse]# m1
-        m2det_samples = np.array(phi['posteriors']['m2det'])[:nObsUse, :nSamplesUse] # m2
-        dl_samples = np.array(phi['posteriors']['dl'])[:nObsUse, :nSamplesUse]*10**3 # dLm distance is given in Gpc in the .h5
-        #theta_samples = np.array(phi['posteriors']['theta']) [:10,:100] # theta
-        # Farr et al.'s simulations: our "observations"
+        
+        if nObsUse is None and nSamplesUse is None:
+            m1det_samples = np.array(phi['posteriors']['m1det'])
+            m2det_samples = np.array(phi['posteriors']['m2det'])
+            dl_samples = np.array(phi['posteriors']['dl'])*10**3 # dLm distance is given in Gpc in the .h5
+            #theta_samples = np.array(phi['posteriors']['theta']) [:10,:100] # theta
+            # Farr et al.'s simulations: our "observations"
+        elif nObsUse is not None and nSamplesUse is None:
+            
+            m1det_samples = np.array(phi['posteriors']['m1det'])[:nObsUse, :]# m1
+            m2det_samples = np.array(phi['posteriors']['m2det'])[:nObsUse, :] # m2
+            dl_samples = np.array(phi['posteriors']['dl'])[:nObsUse, :]*10**3 
+            
+        elif nObsUse is  None and nSamplesUse is not None:
+            
+            m1det_samples = np.array(phi['posteriors']['m1det'])[:, :nSamplesUse]# m1
+            m2det_samples = np.array(phi['posteriors']['m2det'])[:, :nSamplesUse] # m2
+            dl_samples = np.array(phi['posteriors']['dl'])[:, :nSamplesUse]*10**3 
+            
+        elif nObsUse is not None and nSamplesUse is not None:
+            
+            m1det_samples = np.array(phi['posteriors']['m1det'])[:nObsUse, :nSamplesUse]# m1
+            m2det_samples = np.array(phi['posteriors']['m2det'])[:nObsUse, :nSamplesUse] # m2
+            dl_samples = np.array(phi['posteriors']['dl'])[:nObsUse, :nSamplesUse]*10**3 
+            
         
     return np.array([m1det_samples, m2det_samples, dl_samples])
 
 
 
-def load_injections_data():
+def load_injections_data(dataset_name_injections):
+    if dataset_name_injections=='mock':
+        return load_injections_data_mock()
+    elif dataset_name_injections=='LVC':
+        return load_injections_data_LVC()
+    
+  
+    
+def load_injections_data_LVC():
+    return None
+    
+
+def load_injections_data_mock():   
+    
+    
     with h5py.File(os.path.join(dataPath,'selected.h5'), 'r') as f:
-        m1_sel = np.array(f['m1det'])[:nInjUse]
-        m2_sel = np.array(f['m2det'])[:nInjUse]
-        dl_sel = np.array(f['dl'])[:nInjUse]*10**3
-        weights_sel = np.array(f['wt'])[:nInjUse]
+        
+        if nInjUse is not None:
+            m1_sel = np.array(f['m1det'])[:nInjUse]
+            m2_sel = np.array(f['m2det'])[:nInjUse]
+            dl_sel = np.array(f['dl'])[:nInjUse]*10**3
+            weights_sel = np.array(f['wt'])[:nInjUse]
+        else:
+            m1_sel = np.array(f['m1det'])#[:nInjUse]
+            m2_sel = np.array(f['m2det'])#[:nInjUse]
+            dl_sel = np.array(f['dl'])*10**3
+            weights_sel = np.array(f['wt'])#[:nInjUse]
+        
         N_gen = f.attrs['N_gen']
         
     return np.array([m1_sel, m2_sel, dl_sel]), weights_sel , N_gen
@@ -48,4 +96,5 @@ def load_injections_data():
 
 
 
-
+def load_O3_data():
+    return None

models.py
@@ -1,172 +1,157 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jan 20 16:38:28 2021
 
 @author: Michi
 """
 
-
-####################
-# This module contains modeling of the BBH mass function and rate
-####################
-
-
+from config import *
 from utils import *
 from dataFarr import *
 import scipy.stats as ss
+from getLambda import get_Lambda
 
 
 
+#####################################################
+#####################################################
 
-#((20, 140), (0.3, 10), (20, 150)) # (-10, 250), (-10, 250), (-10, 250), (-10, 250), (-10, 250), (-10, 250))
-
-
-
-###########################################################################
 print('Loading data...')
-theta = load_mock_data()
+theta = load_data(dataset_name)
 m1z, m2z, dL = theta
 assert (m1z > 0).all()
 assert (m2z > 0).all()
 assert (dL > 0).all()
-theta_sel, weights_sel, N_gen = load_injections_data()
+theta_sel, weights_sel, N_gen = load_injections_data(dataset_name_injections)
+Nobs = theta[0].shape[0]
+Tobs = 2.5
 print('Done data.')
-print('theta shape: %s' %str(theta.shape))
-print('We have %s observations' %theta.shape[1])
-
-###########################################################################
-
-def get_Lambda(Lambda_test, Lambda_ntest):
-    H0, Xi0, mh= Lambda_test
-    n , lambdaRedshift, alpha, beta, ml, sl, sh  = Lambda_ntest
-    Lambda = [H0, Xi0, n, lambdaRedshift,  alpha, beta, ml, sl, mh, sh]
-    return Lambda
+print('theta shape: %s' % str(theta.shape))
+print('We have %s observations' % theta.shape[1])
 
 
-def alphabias(Lambda_test, Lambda_ntest):
+
+#####################################################
+#####################################################
+
+
+
+#####################################################
+
+def Ndet(Lambda_test, Lambda_ntest):
     Lambda = get_Lambda(Lambda_test, Lambda_ntest)
-    
-    xx = dN_dm1zdm2zddL( Lambda, theta_sel)/weights_sel
-    alpha=np.sum(xx)/N_gen
-    s2=np.sum(xx*xx)/N_gen**2
-    sigmaSq = s2 - alpha*alpha/N_gen
-    Neff = alpha*alpha/sigmaSq
-    
-    return alpha, Neff
+    xx = dN_dm1zdm2zddL(Lambda, theta_sel) / weights_sel
+    mu = np.sum(xx) / N_gen
+    s2 = np.sum(xx * xx) / N_gen ** 2
+    sigmaSq = s2 - mu * mu / N_gen
+    Neff = mu * mu / sigmaSq
+    if Neff < 4 * Nobs:
+        print('NEED MORE SAMPLES FOR SELECTION EFFECTS')
+    return (mu, Neff)
 
 
 def logLik(Lambda_test, Lambda_ntest):
-    '''
+    """
     Lambda:
      H0, Xi0, n, gamma, alpha, beta, ml, sl, mh, sh
     
     Returns log likelihood for all data
-    '''
-    #H0, Xi0,  mh = Lambda_test
-    #n, lambdaRedshift, alpha, beta, ml, sl, sh  = Lambda_ntest
-    #Lambda = [H0, Xi0, n, lambdaRedshift,  alpha, beta, ml, sl, mh, sh]
+    """
     Lambda = get_Lambda(Lambda_test, Lambda_ntest)
-    #m1z, m2z, dL = theta
-    lik = dN_dm1zdm2zddL(Lambda, theta) # ( n_obs x n_samples ) 
-    
-    lik/=originalMassPrior(m1z, m2z)
-    lik/=originalDistPrior(dL)
-        
-    return np.log(lik.mean(axis=1)).sum(axis=-1)
+    lik = dN_dm1zdm2zddL(Lambda, theta)
+    lik /= originalMassPrior(m1z, m2z)
+    lik /= originalDistPrior(dL)
+    return np.log(lik.mean(axis=1)).sum(axis=(-1))
 
-#### The mean is taken over axis = 1 (instead of -1)
 
 def log_prior(Lambda_test, priorLimits):
-    #H0, Xi0, mh = Lambda_test
-    #allVariables = flatten2([Lambda_test,])
-    condition=True
-    for i, (limInf, limSup) in enumerate(priorLimits):
-        condition &= limInf<Lambda_test[i]<limSup 
-    if condition: 
-        return 0.0
+    if np.isscalar(Lambda_test):
+        limInf, limSup = priorLimits[0]
+        condition = limInf < Lambda_test < limSup
     else:
-        return -np.inf
+        condition = True
+        for i, (limInf, limSup) in enumerate(priorLimits):
+            condition &= limInf < Lambda_test[i] < limSup
+
+    if condition:
+        return 0.0
+    return -np.inf
 
 
 def log_posterior(Lambda_test, Lambda_ntest, priorLimits):
     lp = log_prior(Lambda_test, priorLimits)
-    Nobs=theta[0].shape[0]
     if not np.isfinite(lp):
-       return -np.inf
-    alphaSel, Neff = alphabias(Lambda_test, Lambda_ntest)
-    return logLik(Lambda_test, Lambda_ntest)-Nobs*np.log(alphaSel)+(3*Nobs+Nobs**2)/(2*Neff) + lp
-
-
-###########################################################################
-
-
-def originalMassPrior(m1z, m2z):
-    return np.ones(m1z.shape)
-
-def originalDistPrior(dL):
-    return np.ones(dL.shape)
+        return -np.inf
+    myNdet, Neff = Ndet(Lambda_test, Lambda_ntest)
+    return logLik(Lambda_test, Lambda_ntest) - myNdet + (3 * Nobs + Nobs ** 2) / (2 * Neff) + lp
 
 
 
-def dN_dm1dm2dz(z,  Lambda, theta) :
-    '''
+
+
+#####################################################
+
+
+def dN_dm1dm2dz(z, Lambda, theta):
+    """
     - theta is an array (m1z, m2z, dL) where m1z, m2z, dL are arrays 
     of the GW posterior samples
      
      Lambda = (H0, Xi0, n, lambdaRedshift, lambdaBBH ) 
      lambdaBBH is the parameters of the BBH mass function 
-    '''
-    
+    """
     m1z, m2z, dL = theta
-    H0, Xi0, n, lambdaRedshift,  alpha, beta, ml, sl, mh, sh  = Lambda
-    lambdaBBH=[ alpha, beta, ml, sl, mh, sh]
-    
-    m1, m2 = m1z/(1+z), m2z/(1+z)
-    #assert (m1 > 0).all()
-    #assert (m2 > 0).all()
-    return redshiftPrior(z, lambdaRedshift, H0)*massPrior(m1, m2, lambdaBBH)
+    H0, Xi0, n, R0, lambdaRedshift, alpha, beta, ml, sl, mh, sh = Lambda
+    lambdaBBH = [alpha, beta, ml, sl, mh, sh]
+    m1, m2 = m1z / (1 + z), m2z / (1 + z)
+    return redshiftPrior(z, lambdaRedshift, H0) * massPrior(m1, m2, lambdaBBH) * R0 * Tobs * 1e-09
 
 
-
-def dN_dm1zdm2zddL(Lambda, theta) :
-    
+def dN_dm1zdm2zddL(Lambda, theta):
     m1z, m2z, dL = theta
-    H0, Xi0, n, lambdaRedshift,  alpha, beta, ml, sl, mh, sh  = Lambda
-    
-    z=z_from_dLGW_fast(dL, H0, Xi0, n)
+    H0, Xi0, n, R0, lambdaRedshift, alpha, beta, ml, sl, mh, sh = Lambda
+    z = z_from_dLGW_fast(dL, H0, Xi0, n)
     if not (z > 0).all():
-        print('Parameters H0, Xi0, n, lambdaRedshift,  alpha, beta, ml, sl, mh, sh :')
-        print(H0, Xi0, n, lambdaRedshift,  alpha, beta, ml, sl, mh, sh)
-        
-        print('dL = %s' %dL[z<0])
+        print('Parameters H0, Xi0, n, R0, lambdaRedshift,  alpha, beta, ml, sl, mh, sh :')
+        print(H0, Xi0, n, R0, lambdaRedshift, alpha, beta, ml, sl, mh, sh)
+        print('dL = %s' % dL[(z < 0)])
         raise ValueError('negative redshift')
-
-    return  dN_dm1dm2dz(z,  Lambda, theta)/(redshiftJacobian(z)*ddL_dz(z, H0, Xi0, n))
-
+    return dN_dm1dm2dz(z, Lambda, theta) / (redshiftJacobian(z) * ddL_dz(z, H0, Xi0, n))
 
 
 def redshiftJacobian(z):
-    return (1+z)**2
+    return (1 + z) ** 2
 
 
-
-def redshiftPrior(z, gamma,H0):
-    '''
+def redshiftPrior(z, gamma, H0):
+    """
     dV/dz *(1+z)^(gamma-1)  [Mpc^3]
-    '''
-    return 4*np.pi*(1+z)**(gamma-1)*((clight/H0)**3)*j(z) 
-### Depends on H0 in the arguments
+    """
+    return 4 * np.pi * (1 + z) ** (gamma - 1) * (clight / H0) ** 3 * j(z)
 
+
+
+
+#####################################################
 
 def massPrior(m1, m2, lambdaBBH):
-    '''
+    """
     lambdaBBH is the array of parameters of the BBH mass function 
-    '''
+    """
     alpha, beta, ml, sl, mh, sh = lambdaBBH
-    return (m1/30)**(-alpha)*(m2/30)**(beta)*f_smooth(m1, ml=ml, sl=sl, mh=mh, sh=sh)*f_smooth(m2, ml=ml, sl=sl, mh=mh, sh=sh)  
-  ### gamma isn't in lambdaBBH
-    
+    return m1 ** (-alpha) * (m2 / m1) ** beta * f_smooth(m1, ml=ml, sl=sl, mh=mh, sh=sh) * f_smooth(m2, ml=ml, sl=sl, mh=mh, sh=sh) * C1(m1, beta, ml) * C2(alpha, ml, mh)
+
+
+def C1(m, beta, ml):
+    if beta != -1:
+        return (1 + beta) / m / (1 - (ml / m) ** (1 + beta))
+    return 1 / m / np.log(m / ml)
+
+
+def C2(alpha, ml, mh):
+    if alpha != 1:
+        return (1 - alpha) / (mh ** (1 - alpha) - ml ** (1 - alpha))
+    return 1 / np.log(mh / ml)
+
+
 def f_smooth(m, ml=5, sl=0.1, mh=45, sh=0.1):
-    return ss.norm().cdf((np.log(m)-np.log(ml))/sl)*(1-ss.norm().cdf((np.log(m)-np.log(mh))/sh))
-## The last sigma is sh, not sl
+    return ss.norm().cdf((np.log(m) - np.log(ml)) / sl) * (1 - ss.norm().cdf((np.log(m) - np.log(mh)) / sh))

runMCMC.py
@@ -51,6 +51,7 @@ import shutil
 
 
 #from dataFarr import *
+from config import *
 from models import *
 from params import Params, PriorLimits
 
@@ -63,27 +64,11 @@ os.environ["OMP_NUM_THREADS"] = "1"
 #  GLOBAL VARIABLES
 ###############################################
 
-dirName  = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-dataPath=os.path.join(dirName, 'data')
 
-fout='runFull'
 
-dataset_name = 'mock'
 
-telegramAlert = False
-
-nChains=50
-max_n=10000
-
-maxNtaus = 150
-checkTauStep = 100
 # Nobs=100
 
-myParams = Params(dataset_name)
-
-params_inference = [ 'H0', 'Xi0', 'mh'] #'lambdaRedshift', 'alpha', 'beta', 'ml', 'sl', 'mh', 'sh']
-
-params_n_inference = [param for param in myParams.allParams if param not in params_inference]
 
 
 labels_param = [ myParams.names[param] for param in params_inference ] #.sort()
@@ -197,30 +182,26 @@ def main():
     	old_tau = np.inf
 
         # Now we'll sample for up to max_n steps
-    	for sample in sampler.sample(pos, iterations=max_n, progress=True, skip_initial_state_check=True):
-        # Only check convergence every 100 steps
-        	if sampler.iteration % 100:
-            	   continue
-            
-            #if telegramAlert:
-            # 	telegram_bot_sendtext("%s: step No.  %s" %(filenameT,sampler.iteration))
+    	for sample in sampler.sample(pos, iterations=max_n, progress=True, skip_initial_state_check=False):
+            # Only check convergence every 100 steps
+            if sampler.iteration % 100:
+                continue
+            else:
+                tau = sampler.get_autocorr_time(tol=0)
+                autocorr[index] = np.mean(tau)
+                index +=1
+                print('Step n %s. Check autocorrelation: ' %(index*100))
+                print(tau)
         
-            # Compute the autocorrelation time so far
-            # Using tol=0 means that we'll always get an estimate even
-            # if it isn't trustworthy
-        	tau = sampler.get_autocorr_time(tol=0)
-        	autocorr[index] = np.mean(tau)
-        	index += 1
- 
-
-    
-        
-        # Check convergence
-        	converged = np.all(tau * 100 < sampler.iteration)
-        	converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
-        	if converged:
-         	   break
-        	old_tau = tau
+                # Check convergence
+                converged = np.all(tau * 100 < sampler.iteration)
+                converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
+                if converged:
+                    print('Chain has converged. Stopping. ')
+                    break
+                else:
+                    print('Chain has not converged yet. ')
+                    old_tau = tau
         
     fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
     samples = sampler.get_chain()