a8380fe563b4ae393456e782414ed3b0b53dacb5
Michele <mancarell@gmail.com>
2021-02-11 16:58:21 +0100

    testing routined added

---
+++ config_template.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 16:45:34 2021
+
+@author: Michi
+"""
+from params import Params
+
+
+
+dataset_name='mock'
+
+dataset_name_injections='mock'
+
+fout='runFullTry'
+
+
+telegramAlert = False
+
+nChains=50
+max_n=1000
+
+maxNtaus = 150
+checkTauStep = 100
+
+
+params_inference = [ 'H0', 'lambdaRedshift', 'alpha', 'beta', 'ml', 'sl', 'mh', 'sh']
+#'lambdaRedshift', 'alpha', 'beta', 'ml', 'sl', 'mh', 'sh']
+
+
+###########
+# For testing
+
+
+nSamplesUse= None #100
+nObsUse=50
+nInjUse=None #100
+
+
+
+
+#####################
+# Don't touch the part below
+#####################
+
+
+myParams = Params(dataset_name)
+params_n_inference = [param for param in myParams.allParams if param not in params_inference]
+
+
+lines=[]
+lines.append('def get_Lambda(Lambda_test, Lambda_ntest):')
+lines.append('    '+ (', ').join(params_inference) + ' = Lambda_test')
+lines.append('    '+ (', ').join(params_n_inference) + ' = Lambda_ntest')
+lines.append('    Lambda = ['+ (', ').join(myParams.allParams) +']')
+lines.append('    return Lambda' )
+
+with open('getLambda.py', 'w') as f:
+    f.write(('\n').join(lines))
+
+
+#def get_Lambda(Lambda_test, Lambda_ntest):
+    
+#    H0, lambdaRedshift, alpha, beta, ml, sl, mh, sh = Lambda_test
+#    Xi0, n, R0   = Lambda_ntest
+    
+#    Lambda = [H0, Xi0, n, R0, lambdaRedshift,  alpha, beta, ml, sl, mh, sh]
+#    return Lambda
---
+++ testAllparams.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 13:59:58 2021
+
+@author: Michi
+"""
+import time
+import os
+from params import Params, PriorLimits
+import argparse
+from utils import *
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = 'serif'
+plt.rcParams["mathtext.fontset"] = "cm"
+import importlib
+
+in_time=time.time()
+
+
+
+perc_variation=20
+npoints=20
+dataset_name='mock'
+
+nObsUse=50
+
+with open('config.py', 'w') as f:
+    f.write("dataset_name='%s'" %dataset_name)
+    f.write("\ndataset_name_injections='%s'" %dataset_name)
+    f.write("\nnObsUse=%s " %nObsUse)
+    f.write("\nnSamplesUse=None  " )
+    f.write("\nnInjUse=None  " )
+    
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--param", default='H0', type=str, required=True)
+FLAGS = parser.parse_args()
+param = FLAGS.param
+
+
+
+fout = 'test_oneVar_Full'
+out_path=os.path.join(dirName, 'results', fout)
+if not os.path.exists(out_path):
+        print('Creating directory %s' %out_path)
+        os.makedirs(out_path)
+else:
+       print('Using directory %s for output' %out_path)
+    
+logfile = os.path.join(out_path, 'logfile_'+param+'.txt') #out_path+'logfile.txt'
+myLog = Logger(logfile)
+sys.stdout = myLog
+sys.stderr = myLog
+
+
+
+#def main():
+    
+    
+    
+    
+
+    
+myParams = Params(dataset_name)
+myPriorLims = PriorLimits()
+    
+print('Parameter: %s' %param)
+    #for param in myParams.allParams:
+        
+if param=='n':
+        print('Skipping n')
+        exit
+else:
+        truth = myParams.trueValues[param]
+        print('True value: %s' %truth)
+        grid = np.concatenate( [np.linspace( myPriorLims.limInf[param], truth-(truth*perc_variation/100)-0.01, 5), np.linspace(truth-(truth*perc_variation/100), truth+(truth*perc_variation/100), npoints) , np.linspace( truth+(truth*perc_variation/100)+0.01, myPriorLims.limSup[param], 5)])
+            
+        params_n_inference = [nparam for nparam in myParams.allParams if nparam!= param]
+
+        Lambda_ntest = np.array([myParams.trueValues[param] for param in params_n_inference])
+        priorLimits  = [ (myPriorLims.limInf[param],myPriorLims.limSup[param] ), ]
+        
+        print('Writing get_Lambda:' )
+        lines=[]
+        lines.append('def get_Lambda(Lambda_test, Lambda_ntest):')
+        lines.append('    '+ param + ' = Lambda_test')
+        lines.append('    '+ (', ').join(params_n_inference) + ' = Lambda_ntest')
+        lines.append('    Lambda = ['+ (', ').join(myParams.allParams) +']')
+        lines.append('    return Lambda' )
+        print(('\n').join(lines) )
+        
+        with open('getLambda'+param+'.py', 'w') as f:
+            f.write(('\n').join(lines))
+        
+        fread= open('models.py', 'r')
+        fwrite=open('models'+param+'.py', 'w')
+        for line in fread.readlines():
+            ll=line
+            if 'getLambda' in line:
+                ll = 'from getLambda'+param+' import *'
+            fwrite.write(ll+"\n")
+        fwrite.close()
+        fread.close()
+        
+        #from models import log_posterior
+        mymodels = importlib.import_module('models'+param, package=None)
+        
+        
+        print('Computing posterior in range (%s, %s) on %s points... ' %(grid.min(), grid.max(), grid.shape[0] ) )
+        logPosterior = np.array( [mymodels.log_posterior(val, Lambda_ntest, priorLimits) for val in grid ] )
+        posterior = np.exp(logPosterior)
+        posterior/=np.trapz(posterior, grid) 
+        print('Done.')
+        np.savetxt( os.path.join(out_path, param+'_values.txt') , np.stack([grid, posterior], axis=1) )
+        
+        
+        plt.plot(grid, posterior)
+        plt.xlabel(myParams.names[param]);
+        plt.ylabel(r'$p$');
+        plt.axvline(truth, ls='--', color='k', lw=2);
+        plt.savefig( os.path.join(out_path, param+'_post.pdf'))
+        plt.close()
+            
+
+######
+print('\nDone in %.2fs' %(time.time() - in_time))
+    
+sys.stdout = sys.__stdout__
+sys.stderr = sys.__stderr__
+myLog.close() 
+
+            
+#if __name__=='__main__':
+#    main()
