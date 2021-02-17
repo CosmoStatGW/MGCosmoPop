mage units an option; correct log alpha bias

cosmo.py
@@ -12,8 +12,9 @@ import numpy as np
 from astropy.cosmology import FlatwCDM, FlatLambdaCDM
 from scipy import interpolate
 #from astropy import constants as const
-#import astropy.units as u
+import astropy.units as u
 from scipy.optimize import fsolve
+
 
 
 cosmoGlobals = FlatwCDM(H0=Globals.H0GLOB, Om0=Globals.Om0GLOB, w0=-1)
@@ -31,9 +32,9 @@ def uu(z, Om, w0):
     Dimensionless comoving distance. Does not depend on H0
     '''
     if w0!=-1:
-        return 70/Globals.clight*FlatwCDM(H0=70, Om0=Om, w0=w0, Neff=0).comoving_distance(z).value
+        return 70/Globals.clight*FlatwCDM(H0=70, Om0=Om, w0=w0, Neff=0).comoving_distance(z).to(u.Mpc).value
     else:
-        return 70/Globals.clight*FlatLambdaCDM(H0=70, Om0=Om).comoving_distance(z).value
+        return 70/Globals.clight*FlatLambdaCDM(H0=70, Om0=Om).comoving_distance(z).to(u.Mpc).value
 
 def E(z, Om, w0):
     '''
@@ -56,13 +57,15 @@ def dV_dz(z, H0, Om, w0):
     Jacobian of comoving volume, with correct dimensions [Mpc^3]. Depends on H0
     '''
     if w0!=-1:
-        return 4*np.pi*FlatwCDM(H0=H0, Om0=Om, w0=w0, Neff=0).differential_comoving_volume(z).value
+        return 4*np.pi*FlatwCDM(H0=H0, Om0=Om, w0=w0, Neff=0).differential_comoving_volume(z).to(Globals.which_unit**3/u.sr).value
     else:
-        return 4*np.pi*FlatLambdaCDM(H0=H0, Om0=Om,).differential_comoving_volume(z).value
+        return 4*np.pi*FlatLambdaCDM(H0=H0, Om0=Om,).differential_comoving_volume(z).to(Globals.which_unit**3/u.sr).value
 
 def log_dV_dz(z, H0, Om0, w0):
-        return np.log(4*np.pi)+3*np.log(Globals.clight)-3*np.log(H0)+2*np.log(uu(z, Om0, w0))-np.log(E(z, Om0, w0))
-    
+    res =  np.log(4*np.pi)+3*np.log(Globals.clight)-3*np.log(H0)+2*np.log(uu(z, Om0, w0))-np.log(E(z, Om0, w0))
+    if Globals.which_unit==u.Gpc:
+        res -=9*np.log(10)
+    return res
 
 
 def s(z, Xi0, n):
@@ -77,12 +80,16 @@ def ddL_dz(z, H0, Om, w0, Xi0, n):
     '''
      Jacobian d(DL)/dz  [Mpc]
     '''
+    if Globals.which_unit==u.Gpc:
+        H0*=1e03
     return (sPrime(z, Xi0, n)*uu(z, Om, w0)+s(z, Xi0, n)/E(z, Om, w0))*(Globals.clight/H0)
 
 
 def log_ddL_dz(z, H0, Om0, w0, Xi0, n):
-    return np.log(Globals.clight)-np.log(H0)+np.log(sPrime(z, Xi0, n)*uu(z, Om0, w0)+s(z, Xi0, n)/E(z, Om0, w0))
-
+    res =  np.log(Globals.clight)-np.log(H0)+np.log(sPrime(z, Xi0, n)*uu(z, Om0, w0)+s(z, Xi0, n)/E(z, Om0, w0))
+    if Globals.which_unit==u.Gpc:
+        res -= 3*np.log(10)
+    return res
 
 def dLGW(z, H0, Om, w0, Xi0, n):
     '''                                                                                                          

data.py
@@ -2,8 +2,8 @@ import numpy as np
 import h5py
 import os
 from config import nObsUse, nSamplesUse, nInjUse
-from glob import *
-
+import Globals
+import astropy.units as u
 
 
 
@@ -40,32 +40,35 @@ def load_data(dataset_name):
 
 
 def load_mock_data():
-    with h5py.File(os.path.join(dataPath,'observations.h5'), 'r') as phi: #observations.h5 has to be in the same folder as this code
+    with h5py.File(os.path.join(Globals.dataPath,'observations.h5'), 'r') as phi: #observations.h5 has to be in the same folder as this code
         
         if nObsUse is None and nSamplesUse is None:
             m1det_samples = np.array(phi['posteriors']['m1det'])
             m2det_samples = np.array(phi['posteriors']['m2det'])
-            dl_samples = np.array(phi['posteriors']['dl'])*10**3 # dLm distance is given in Gpc in the .h5
+            dl_samples = np.array(phi['posteriors']['dl']) # dLm distance is given in Gpc in the .h5
             #theta_samples = np.array(phi['posteriors']['theta']) [:10,:100] # theta
             # Farr et al.'s simulations: our "observations"
         elif nObsUse is not None and nSamplesUse is None:
             
             m1det_samples = np.array(phi['posteriors']['m1det'])[:nObsUse, :]# m1
             m2det_samples = np.array(phi['posteriors']['m2det'])[:nObsUse, :] # m2
-            dl_samples = np.array(phi['posteriors']['dl'])[:nObsUse, :]*10**3 
+            dl_samples = np.array(phi['posteriors']['dl'])[:nObsUse, :] 
             
         elif nObsUse is  None and nSamplesUse is not None:
             
             m1det_samples = np.array(phi['posteriors']['m1det'])[:, :nSamplesUse]# m1
             m2det_samples = np.array(phi['posteriors']['m2det'])[:, :nSamplesUse] # m2
-            dl_samples = np.array(phi['posteriors']['dl'])[:, :nSamplesUse]*10**3 
+            dl_samples = np.array(phi['posteriors']['dl'])[:, :nSamplesUse]
             
         elif nObsUse is not None and nSamplesUse is not None:
             
             m1det_samples = np.array(phi['posteriors']['m1det'])[:nObsUse, :nSamplesUse]# m1
             m2det_samples = np.array(phi['posteriors']['m2det'])[:nObsUse, :nSamplesUse] # m2
-            dl_samples = np.array(phi['posteriors']['dl'])[:nObsUse, :nSamplesUse]*10**3 
-            
+            dl_samples = np.array(phi['posteriors']['dl'])[:nObsUse, :nSamplesUse] 
+    
+    if Globals.which_unit==u.Mpc:
+        print('Using distances in Mpc')
+        dl_samples*=1e03
     theta =   np.array([m1det_samples, m2det_samples, dl_samples])
     return theta, np.count_nonzero(theta, axis=-1)
 
@@ -86,21 +89,22 @@ def load_injections_data_LVC():
 def load_injections_data_mock():   
     
     
-    with h5py.File(os.path.join(dataPath,'selected.h5'), 'r') as f:
+    with h5py.File(os.path.join(Globals.dataPath,'selected.h5'), 'r') as f:
         
         if nInjUse is not None:
             m1_sel = np.array(f['m1det'])[:nInjUse]
             m2_sel = np.array(f['m2det'])[:nInjUse]
-            dl_sel = np.array(f['dl'])[:nInjUse]*10**3
+            dl_sel = np.array(f['dl'])[:nInjUse]
             weights_sel = np.array(f['wt'])[:nInjUse]
         else:
             m1_sel = np.array(f['m1det'])
             m2_sel = np.array(f['m2det'])
-            dl_sel = np.array(f['dl'])*10**3
+            dl_sel = np.array(f['dl'])
             weights_sel = np.array(f['wt'])
         
         N_gen = f.attrs['N_gen']
-        
+    if Globals.which_unit==u.Mpc:
+        dl_sel*=1e03
     return np.array([m1_sel, m2_sel, dl_sel]), weights_sel , N_gen
 
 

models.py
@@ -24,6 +24,7 @@ from scipy.special import logsumexp
 print('Loading data...')
 theta, Nsamples = data.load_data(config.dataset_name)
 Nobs = theta[0].shape[0]
+logNobs = np.log(Nobs)
 m1z, m2z, dL = theta
 assert (m1z > 0).all()
 assert (m2z > 0).all()
@@ -65,18 +66,40 @@ def selectionBias(Lambda, m1, m2, z, get_neff=False):
     #Lambda = get_Lambda(Lambda_test, Lambda_ntest)
     #H0, Om0, w0, Xi0, n, R0, lambdaRedshift, alpha, beta, ml, sl, mh, sh = Lambda
     
-    xx = log_dN_dm1zdm2zddL(Lambda, m1, m2, z) - log_weights_sel
+    xx = dN_dm1zdm2zddL(Lambda, m1, m2, z)/weights_sel
         
-    logMu = logsumexp(xx) - logN_gen
+    mu = xx.sum()/N_gen
+    if not get_neff:
+        return mu, np.NaN #np.repeat(np.NaN, logMu.shape[0] )
+    else:
+        muSq = mu*mu
+        SigmaSq = np.sum(xx*xx)/N_gen**2 - muSq/N_gen
+        Neff = muSq/SigmaSq
+        if Neff < 4 * Nobs:
+            print('NEED MORE SAMPLES FOR SELECTION EFFECTS! ') #Values of lambda_test: %s' %str(Lambda_test))
+        return mu, Neff
+
+
+
+def logSelectionBias(Lambda, m1, m2, z, get_neff=False):
+    
+    #m1, m2, z = precomputed['m1'], precomputed['m2'], precomputed['z']
+    #Lambda = get_Lambda(Lambda_test, Lambda_ntest)
+    #H0, Om0, w0, Xi0, n, R0, lambdaRedshift, alpha, beta, ml, sl, mh, sh = Lambda
+    
+    logxx = log_dN_dm1zdm2zddL(Lambda, m1, m2, z) - log_weights_sel   
+    logMu = logsumexp(logxx) - logN_gen
+    
     if not get_neff:
         return logMu, np.NaN #np.repeat(np.NaN, logMu.shape[0] )
     else:
-        muSq = np.exp(2*logMu)
-        logs2 = logsumexp(2*xx) -2*N_gen
-        SigmaSq = np.exp(logs2) - muSq / N_gen
-        Neff = muSq / SigmaSq
+        muSq = np.exp(2*logMu).astype('float128')
+        logs2 = (logsumexp(2*logxx) -2*logN_gen).astype('float128')
+        #logSigmaSq = -logN_gen-2*logMu+logsumexp(2*xx)
+        SigmaSq = np.exp(logs2) - muSq/N_gen
+        Neff = muSq/SigmaSq
         if Neff < 4 * Nobs:
-            print('NEED MORE SAMPLES FOR SELECTION EFFECTS! ') #Values of lambda_test: %s' %str(Lambda_test))
+            print('NEED MORE SAMPLES FOR SELECTION EFFECTS! ')#Values of lambda_test: %s' %str(Lambda_test))
         return logMu, Neff
 
 