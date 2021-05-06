29947729ac0cfd3fb1f400bdd48c3a1051a885f7
Michele <mancarell@gmail.com>
2021-05-06 09:44:46 +0200

    removed prind directory, annoying in parallel runs

--- MGMG/Globals.py
+++ MGMG/Globals.py
@@ -7,14 +7,15 @@ Created on Fri Feb 12 17:37:48 2021
 """
 import os
 
-dirName  = os.path.join( os.path.dirname(os.path.abspath(__file__)), '..' )
-print('Directory: %s' %dirName)
+#dirName  = os.path.join( os.path.dirname(os.path.abspath(__file__)), '..' )
+dirName  = os.path.dirname( os.path.dirname(os.path.abspath(__file__)))
+#print('Directory: %s' %dirName)
 dataPath=os.path.join(dirName, 'data')
-print('Data Directory: %s' %dataPath)
+#print('Data Directory: %s' %dataPath)
 
 detectorPath=os.path.join(dataPath, 'detectors')
-print('SNR Directory: %s' %detectorPath)
+#print('SNR Directory: %s' %detectorPath)
 
 
 thetaPath=os.path.join(dataPath, 'theta')
-print('theta Directory: %s' %thetaPath)
+#print('theta Directory: %s' %thetaPath)
