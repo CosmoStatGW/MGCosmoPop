4fb76e043d6425d048d4c263e3a73e52320ad8e2
Michele <mancarell@gmail.com>
2022-03-07 14:59:43 +0100

    requirements and setup fixed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
 
-    name='MGCosmoPop'
-    version='1.0.0'
-    author='Michele Mancarella'
-    author_email='michele.mancarella@unige.ch'
-    description='Cosmology and Modified Gravity with Population models'
-    url='https://github.com/CosmoStatGW/MGCosmoPop'
-    packages=find_packages()
-    python_requires='>=3.7'
-    license='BSD-3-Clause License'
+    name='MGCosmoPop',
+    version='1.0.0',
+    author='Michele Mancarella',
+    author_email='michele.mancarella@unige.ch',
+    description='Cosmology and Modified Gravity with Population models',
+    url='https://github.com/CosmoStatGW/MGCosmoPop',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    license='BSD-3-Clause License',
 )
